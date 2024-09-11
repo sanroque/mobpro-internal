edd515aea1bdb35631b57a0f9652b84838e9cc20
sanroque <goncalo91machado@gmail.com>
2024-09-11 16:21:01 +0100

    feat: add lombok anotations

--- src/main/java/com/mobpro/mobpro_internal/dto/UserDTO.java
+++ src/main/java/com/mobpro/mobpro_internal/dto/UserDTO.java
@@ -2,8 +2,14 @@ package com.mobpro.mobpro_internal.dto;
 
 import com.mobpro.mobpro_internal.entity.User;
 import com.mobpro.mobpro_internal.entity.UserRole;
+import lombok.*;
 import org.springframework.beans.BeanUtils;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class UserDTO {
 
     private Long id;
