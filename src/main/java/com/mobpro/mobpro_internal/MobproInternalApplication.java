89802cb81319e6874a0114e90ec85482d973dfb1
sanroque <goncalo91machado@gmail.com>
2024-09-11 11:04:59 +0100

    feat: first commit

---
+++ src/main/java/com/mobpro/mobpro_internal/MobproInternalApplication.java
@@ -0,0 +1,13 @@
+package com.mobpro.mobpro_internal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobproInternalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobproInternalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobpro/mobpro_internal/MobproInternalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobpro.mobpro_internal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobproInternalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
