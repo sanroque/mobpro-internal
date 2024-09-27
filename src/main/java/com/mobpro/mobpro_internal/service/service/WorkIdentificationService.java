f71eef1d8d548f77663423e16c1df415e11d6a46
sanroque <goncalo91machado@gmail.com>
2024-09-27 16:49:55 +0100

    feat: updates #4

--- src/main/java/com/mobpro/mobpro_internal/service/service/WorkIdentificationService.java
+++ src/main/java/com/mobpro/mobpro_internal/service/service/WorkIdentificationService.java
@@ -8,8 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @RequiredArgsConstructor(onConstructor_ = @__(@Autowired))
 @Service
 public class WorkIdentificationService {
